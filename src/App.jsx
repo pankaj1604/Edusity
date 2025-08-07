a64fe6034c528cd7381606fa9759752792102deb
Pankaj Kumar <dev.pankajkr7@gmail.com>
2025-08-07 14:25:37 +0530

    Added Title and About Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
 
 const App = () => {
   return (
@@ -9,8 +11,9 @@ const App = () => {
       <Navbar></Navbar>
       <Hero></Hero>
       <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer'></Title>
       <Programs></Programs>
-
+      <About></About>
       </div>
     </div>
   )
