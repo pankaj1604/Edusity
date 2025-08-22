54b86555271ef2bd59f3af5e29de49717cc7d3f1
Pankaj Kumar <dev.pankajkr7@gmail.com>
2025-08-22 21:15:21 +0530

    Added Contact Us and Footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,60 @@
 import React from 'react'
 import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "4e598c1e-66ed-47d3-8342-3a90d74d06ac");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className='contact'>
       <div className="contact-col">
-        <h3>Send us a message</h3>
+        <h3>Send us a message <img src={msg_icon}/></h3>
         <p>Feel free to reach out through the contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceoptional service to our university community.</p>
         <ul>
-            <li>Conatct@edusity.com</li>
-            <li>+1 123-456-7890</li>
-            <li>77 Massachusetts Ave, Cambridge<br /> MA 02139, United States</li>
+            <li><img src={mail_icon} alt="" />Conatct@edusity.com</li>
+            <li><img src={phone_icon} alt="" />+1 123-456-7890</li>
+            <li><img src={location_icon} alt="" />77 Massachusetts Ave, Cambridge<br /> MA 02139, United States</li>
         </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your Name</label>
+          <input type="text" name='name' placeholder='Enter your name' required />
+          <label>Phone Number</label>
+          <input type="tel" name="phone" placeholder='Enter your mobile number' required/>
+          <label>Write your message here</label>
+          <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+          <button type='submit' className='btn dark-btn'>Submit Now <img src={white_arrow} alt="" /></button>
+        </form>
+        <span>{result}</span>
       </div>
     </div>
   )
